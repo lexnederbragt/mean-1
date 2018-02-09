d21dac5931ce9f22e8340b3963b7fedd066a12f4
hannahberg <hannah.brg@gmail.com>
2018-02-09 14:49:58 +0100

    added raise error for complex numbers

---
+++ mean_mod.py
@@ -0,0 +1,12 @@
+medef makeavg(num_list):
+    try:
+        mean = sum(num_list)/len(num_list
+        if isinstance(mean, complex):
+            return NotImplemented
+        return mean
+    except ZeroDivisionError as detail:
+        msg = "\n Cannot compute the mean value of an empty list"
+        raise ZeroDivisionError(detail.__str__() + msg)
+    except TypeError as detail:
+        msg = "\n Please pass a list of numbers, not strings"
+        raise TypeError(detail.__str__() + msg)
